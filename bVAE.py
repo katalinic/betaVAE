4f69276cabccde502325be3b3cf58694add118b0
katalinic <katalinic@users.noreply.github.com>
2018-01-27 09:24:05 +0000

    Initial commit

---
+++ bVAE.py
@@ -0,0 +1,43 @@
+import tensorflow as tf
+import numpy as np
+
+def encoder(x, num_latents):
+    with tf.variable_scope("encoder"):
+        enc_hid1 = tf.contrib.layers.fully_connected(x, 1200, activation_fn=tf.nn.relu)
+        enc_hid2 = tf.contrib.layers.fully_connected(x, 1200, activation_fn=tf.nn.relu)
+        mean = tf.contrib.layers.fully_connected(enc_hid2, num_latents, activation_fn=None)
+        #activation on latent not specified?
+        var = tf.contrib.layers.fully_connected(enc_hid2, num_latents, activation_fn=tf.nn.softplus)+1e-6
+        # var = tf.contrib.layers.fully_connected(enc_hid2, num_latents, activation_fn=tf.nn.relu)+1e-6
+    return mean, var
+
+def decoder(z, reuse=False):
+    with tf.variable_scope("decoder", reuse=reuse):
+        dec_hid1 = tf.contrib.layers.fully_connected(z, 1200, activation_fn=tf.nn.tanh)
+        dec_hid2 = tf.contrib.layers.fully_connected(dec_hid1, 1200, activation_fn=tf.nn.tanh)
+        dec_hid3 = tf.contrib.layers.fully_connected(dec_hid2, 1200, activation_fn=tf.nn.tanh)
+        dec_out = tf.contrib.layers.fully_connected(dec_hid3, 4096, activation_fn=tf.nn.sigmoid) #testing sigmoid instead of tanh
+        # dec_out = tf.nn.sigmoid(dec_out)
+        dec_out = tf.clip_by_value(dec_out, 1e-8, 1-1e-8)
+    return dec_out
+
+def enc_dec_connection(x, num_latents):
+    mean, var = encoder(x, num_latents)
+    eps = tf.random_normal(tf.shape(mean))
+    non_sampled_z = mean + var*eps
+    dec_out = decoder(non_sampled_z)
+    return mean, var, dec_out
+
+def optimisation(mean, var, y, decoder_output, beta, lr):
+    ce = tf.reduce_mean(tf.reduce_sum(y*tf.log(decoder_output)+(1-y)*tf.log(1-decoder_output),axis=1))
+    kl = -beta*tf.reduce_mean(0.5*tf.reduce_sum((1+2*tf.log(var)-(tf.square(var)+tf.square(mean))),axis=1))
+    loss = ce - kl
+    optimizer = tf.train.AdagradOptimizer(learning_rate = lr)
+    optimise = optimizer.minimize(-loss)
+    return -loss, optimise
+
+def build_VAE(x, y, z, num_latents, beta, lr):
+    mean, var, dec_out = enc_dec_connection(x, num_latents)
+    loss, optimise = optimisation(mean, var, y, dec_out, beta, lr)
+    sample_dec_out = decoder(z, reuse=True)
+    return loss, optimise, mean, var, sample_dec_out
---
+++ main.py
@@ -0,0 +1,97 @@
+import tensorflow as tf
+import numpy as np
+import matplotlib.pyplot as plt
+import bVAE
+import sprites
+import os
+import time
+import sys
+
+num_epochs = 20
+beta = 4
+BATCH_SIZE = 32
+lr = 1e-2
+num_samples = 10 #number of samples to take
+enc_num_input = 4096
+dec_num_output = 4096
+num_latents = 10
+training = True
+restore = False
+
+#Build graph
+x = tf.placeholder(tf.float32, [None, enc_num_input])
+y = tf.placeholder(tf.float32, [None, dec_num_output])
+z = tf.placeholder(tf.float32, [None, num_latents])
+loss, optimise, mean, var, sample_dec_out = bVAE.build_VAE(x, y, z, num_latents, beta, lr)
+
+# Training.
+if training:
+    print ("Loading sprites.")
+    train_sprites, test_sprites = sprites.prepare_sprites()
+    #temporary subset
+    # train_sprites = train_sprites[:100000]
+    print ("Training commenced.")
+    with tf.Session() as sess:
+        sess.run(tf.global_variables_initializer())
+        if restore:
+            # Load model.
+            saver = tf.train.Saver()
+            saver.restore(sess, './model/model.checkpoint')
+        for ep in range(num_epochs):
+            start_time = time.time()
+            np.random.shuffle(train_sprites)
+            for i in range(int(train_sprites.shape[0]//BATCH_SIZE)):
+                batch_xs = train_sprites[i*BATCH_SIZE:(i+1)*BATCH_SIZE]
+                batch_loss, _ = sess.run([loss, optimise], feed_dict = {x: batch_xs, y: batch_xs})
+                moving_loss = batch_loss if i==0 else 0.99*moving_loss+0.01*batch_loss
+                sys.stdout.write(str(moving_loss) + ' ' + str(batch_loss)+'\r')
+                # sys.stdout.flush()
+            test_loss = sess.run(loss, feed_dict = {x: test_sprites, y: test_sprites})
+            print ('Epoch : {}, Loss: {}, Time: {}'.format(ep, test_loss, time.time()-start_time))
+
+            #visualise samples
+            num_latents = 10
+
+            sampling_input = np.eye(num_latents)
+            sampling_input = np.random.multivariate_normal(np.zeros(num_latents),np.eye(num_latents),num_samples).reshape(-1,num_latents)
+            image_samples = sess.run(sample_dec_out, feed_dict = {z : sampling_input})
+            fig = plt.figure(figsize=(10,10))
+            for n in range(1,num_samples+1):
+                ax = plt.subplot(int(num_samples/2),2,n)
+                ax.imshow(image_samples[n-1].reshape(64,64),cmap='gray')
+            plt.suptitle("Sampled digits")
+            if not os.path.exists('./plots/'):
+                os.mkdir('./plots/')
+            plt.savefig('plots/sampled_sprites_e{}.png'.format(ep))
+
+            # Save model.
+            if not os.path.exists('./model/'):
+                os.mkdir('./model/')
+            saver = tf.train.Saver()
+            saver.save(sess, './model/model.checkpoint')
+
+else:
+    print ("Creating latent traversal.")
+    with tf.Session() as sess:
+        # Load model.
+        saver = tf.train.Saver()
+        saver.restore(sess, './model/model.checkpoint')
+
+        image = sprites.load_sprites()[0].reshape(1,4096)
+        _mean = sess.run(mean, feed_dict = {x : image})
+        varied_mean = np.arange(-3, 3.01, 1)
+        num_variations = len(varied_mean)
+
+        # Generate variation images.
+        if not os.path.exists('./plots/'):
+            os.mkdir('./plots/')
+        fig = plt.figure(figsize=(10,10))
+        for latent in range(num_latents):
+            all_means = np.tile(_mean, num_variations).reshape(num_variations, -1)
+            all_means[:,latent] = varied_mean
+            image_samples = sess.run(sample_dec_out, feed_dict = {z : all_means})
+            for n in range(1,num_variations+1):
+                ax = plt.subplot(num_variations,1,n)
+                ax.imshow(image_samples[n-1].reshape(64,64),cmap='gray')
+            plt.suptitle("Sprite latent traversal")
+            plt.savefig('plots/sprites2_e{}.png'.format(latent))
