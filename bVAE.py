ed02d4e921bf3ae5c83674f84cdccc0587f1701a
katalinic <katalinic@users.noreply.github.com>
2018-02-02 10:25:04 +0000

    output and loss stability changes

--- bVAE.py
+++ bVAE.py
@@ -6,7 +6,7 @@ def encoder(x, num_latents):
         enc_hid1 = tf.contrib.layers.fully_connected(x, 1200, activation_fn=tf.nn.relu)
         enc_hid2 = tf.contrib.layers.fully_connected(enc_hid1, 1200, activation_fn=tf.nn.relu)
         mean = tf.contrib.layers.fully_connected(enc_hid2, num_latents, activation_fn=None)
-        var = tf.contrib.layers.fully_connected(enc_hid2, num_latents, activation_fn=tf.nn.softplus)+1e-6
+        var = tf.contrib.layers.fully_connected(enc_hid2, num_latents, activation_fn=None)
     return mean, var
 
 def decoder(z, reuse=False):
@@ -14,24 +14,23 @@ def decoder(z, reuse=False):
         dec_hid1 = tf.contrib.layers.fully_connected(z, 1200, activation_fn=tf.nn.tanh)
         dec_hid2 = tf.contrib.layers.fully_connected(dec_hid1, 1200, activation_fn=tf.nn.tanh)
         dec_hid3 = tf.contrib.layers.fully_connected(dec_hid2, 1200, activation_fn=tf.nn.tanh)
-        dec_out = tf.contrib.layers.fully_connected(dec_hid3, 4096, activation_fn=tf.nn.sigmoid)
-        dec_out = tf.clip_by_value(dec_out, 1e-8, 1-1e-8)
+        dec_out = tf.contrib.layers.fully_connected(dec_hid3, 4096, activation_fn=None)
     return dec_out
 
 def enc_dec_connection(x, num_latents):
     mean, var = encoder(x, num_latents)
     eps = tf.random_normal(tf.shape(mean))
-    non_sampled_z = mean + var*eps
+    non_sampled_z = mean + tf.sqrt(tf.exp(var))*eps
     dec_out = decoder(non_sampled_z)
     return mean, var, dec_out
 
 def optimisation(mean, var, y, decoder_output, beta, lr):
-    ce = tf.reduce_mean(tf.reduce_sum(y*tf.log(decoder_output)+(1-y)*tf.log(1-decoder_output),axis=1))
-    kl = -beta*tf.reduce_mean(0.5*tf.reduce_sum((1+2*tf.log(var)-(tf.square(var)+tf.square(mean))),axis=1))
-    loss = ce - kl
+    ce = tf.reduce_mean(tf.reduce_sum(tf.nn.sigmoid_cross_entropy_with_logits(labels=y,logits=decoder_output),axis=1))
+    kl = -beta*tf.reduce_mean(0.5*tf.reduce_sum((1+var-(tf.exp(var)+tf.square(mean))),axis=1))
+    loss = ce + kl
     optimizer = tf.train.AdagradOptimizer(learning_rate = lr)
-    optimise = optimizer.minimize(-loss)
-    return -loss, optimise
+    optimise = optimizer.minimize(loss)
+    return loss, optimise
 
 def build_VAE(x, y, z, num_latents, beta, lr):
     mean, var, dec_out = enc_dec_connection(x, num_latents)
