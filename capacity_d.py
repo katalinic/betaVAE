16ed2699ed708840dca31b55f7e1e48f782ddfa5
katalinic <katalinic.goran.ml@gmail.com>
2018-12-05 16:43:07 +0000

    add dataset API based capacity version, clean placeholder-based version

--- capacity_d.py
+++ capacity_d.py
@@ -12,31 +12,52 @@ flags.DEFINE_integer("gamma", 100, "Capacity constraint coefficient.")
 flags.DEFINE_float("std_init", 1e-1, "Weight initialisation std.")
 flags.DEFINE_integer("batch_size", 64, "Batch size.")
 flags.DEFINE_integer("num_latents", 5, "Latent dimensionality.")
+flags.DEFINE_integer("epochs", 26, "Training epochs.")
 
 DATASET_PATH = 'dsprites_ndarray_co1sh3sc6or40x32y32_64x64.npz'
+TEST_SIZE = 5000
 
-def load_sprites(only_images=True):
-    dataset_zip = np.load(DATASET_PATH, encoding='latin1')
-    imgs = dataset_zip['imgs'].reshape(-1, 4096)
-    if only_images:
-        return imgs
-    else:
-        latents = dataset_zip['latents_values']
+
+class SpritesDataset():
+    def __init__(self):
+        dataset_zip = np.load(DATASET_PATH, encoding='latin1')
+        imgs, latents = dataset_zip['imgs'], dataset_zip['latents_values']
+        self.imgs = imgs.reshape(-1, 4096)
         latents = latents[:, 1:6]
-        # Normalise factors.
         latents = (latents - np.mean(latents, axis=0)) / np.std(latents, axis=0)
-        images_and_latents = np.concatenate((latents, imgs), axis=1)
-        return images_and_latents
+        # Shuffle initially for train, test split since input is ordered.
+        inds = np.arange(len(self))
+        np.random.shuffle(inds)
+        self.imgs = self.imgs[inds]
+        self.latents = latents[inds]
 
-def prepare_sprites():
-    images_and_latents = load_sprites(False)
-    inds = np.arange(images_and_latents.shape[0])
-    np.random.shuffle(inds)
-    # Assumed 5000 test examples.
-    test_inds = inds[:5000]
-    train_inds = inds[5000:]
-    return images_and_latents[train_inds], images_and_latents[test_inds]
+    def __len__(self):
+        return len(self.imgs)
 
+    def gen(self):
+        for i in range(0, len(self), FLAGS.batch_size):
+            yield self.imgs[i:i + FLAGS.batch_size], self.latents[i:i + FLAGS.batch_size]
+
+
+def input_pipeline(gen):
+    dataset = tf.data.Dataset()
+    dataset = dataset.from_generator(gen, output_types=(tf.uint8, tf.float32),
+        output_shapes=((FLAGS.batch_size, 4096), (FLAGS.batch_size, FLAGS.num_latents)))
+    train_dataset = dataset.skip(TEST_SIZE // FLAGS.batch_size)
+    test_dataset = dataset.take(TEST_SIZE // FLAGS.batch_size)
+
+    train_dataset = train_dataset.shuffle(buffer_size=5000)
+    train_dataset = train_dataset.repeat(FLAGS.epochs)
+    train_dataset = train_dataset.prefetch(1)
+    train_iter = train_dataset.make_initializable_iterator()
+    next_img_latent_pair = train_iter.get_next()
+
+    test_dataset = test_dataset.repeat()  # Intentional infinite repeat.
+    test_dataset = test_dataset.batch(TEST_SIZE // FLAGS.batch_size)
+    test_iter = test_dataset.make_initializable_iterator()
+    next_test_img_latent_pair = test_iter.get_next()
+
+    return next_img_latent_pair, next_test_img_latent_pair, train_iter, test_iter
 
 def encoder(x):
     with tf.variable_scope("encoder", reuse=tf.AUTO_REUSE):
@@ -111,8 +132,16 @@ def build_VAE(x, y, C):
 def train():
     tf.reset_default_graph()
 
-    f = tf.placeholder(tf.float32, [None, FLAGS.num_latents])
-    y = tf.placeholder(tf.float32, [None, 4096])
+    sprites = SpritesDataset()
+    # Train and test Datasets.
+    next_img_latent_pair, next_test_img_latent_pair, train_iter, test_iter = \
+        input_pipeline(sprites.gen)
+    train_img, train_latent = next_img_latent_pair
+    train_img = tf.to_float(train_img)
+    test_img, test_latent = next_test_img_latent_pair
+    test_img = tf.to_float(test_img)
+    test_img = tf.reshape(test_img, (-1, 4096))
+    test_latent = tf.reshape(test_latent, (-1, FLAGS.num_latents))
 
     # Train op counter.
     ep = tf.get_variable(
@@ -124,36 +153,34 @@ def train():
     # Capacity.
     C = tf.maximum(0., ep - 20000.) * 0.75/10000. + 0.5
 
-    loss, kl_per_latent = build_VAE(f, y, C)
+    loss, _ = build_VAE(train_latent, train_img, C)
     optimise = build_optimisation(loss)
     optimise = tf.group(optimise, ep_increment)
 
-    raw_train, raw_test = prepare_sprites()
+    # Test loss.
+    _, kl_per_latent = build_VAE(test_latent, test_img, C)
+
+    init = tf.group(
+        tf.global_variables_initializer(),
+        train_iter.initializer,
+        test_iter.initializer)
 
     with tf.Session() as sess:
-        sess.run(tf.global_variables_initializer())
+        sess.run(init)
         j = 0
-        test_feed_dict = {f: raw_test[:, :FLAGS.num_latents],
-                          y: raw_test[:, FLAGS.num_latents:]}
         start_time = time.time()
-        for ep in range(26):
-
-            np.random.shuffle(raw_train)
-            train_sprites = raw_train[:, FLAGS.num_latents:]
-            train_latents = raw_train[:, :FLAGS.num_latents]
-
-            for i in range(int(train_sprites.shape[0]//FLAGS.batch_size)):
-                j += 1
-                if j > 0 and j % 6000 == 0:
-                    k = sess.run(kl_per_latent, feed_dict=test_feed_dict)
-                    print('Iteration: {}, KL: {}, Time: {:.2f}'.format(
-                        j, np.round(np.mean(k, 0), 1), time.time() - start_time))
-                    start_time = time.time()
-
-                train_feed_dict = {
-                    f: train_latents[i * FLAGS.batch_size:(i + 1) * FLAGS.batch_size],
-                    y: train_sprites[i * FLAGS.batch_size: (i + 1) * FLAGS.batch_size]}
-                sess.run(optimise, feed_dict=train_feed_dict)
+        while True:
+            try:
+                sess.run(optimise)
+            except tf.errors.OutOfRangeError:
+                break
+            j += 1
+            # Evaluate every 6k training iterations.
+            if j % 6000 == 0:
+                k = sess.run(kl_per_latent)
+                print('Iteration: {}, KL: {}, Time: {:.2f}'.format(
+                    j, np.round(np.mean(k, 0), 1), time.time() - start_time))
+                start_time = time.time()
 
 
 def main(_):
